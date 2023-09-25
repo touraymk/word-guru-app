5e1155e71d1b63897bcbe6e2e57607621b251584
touraymk <131713898+touraymk@users.noreply.github.com>
2023-09-26 00:12:00 +0200

    Update App.js
    Added footer with link to personal website and GitHub repository.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer className="text-center">
+        <small>
+          Coded with <span className="emojiColor">♥️</span> by{" "}
+          <a href="https://monumental-malasada-41cd9f.netlify.app">
+            Mariama Touray
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/touraymk/dictionary-react-app">
+            open-sourced
+          </a>
+        </small>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
